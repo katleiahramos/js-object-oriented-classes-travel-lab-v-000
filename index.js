aaae64c760ad100b133f0bb21baa74561a0e33b3
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 16:32:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class Driver{
 
 class Route{
   constructor(beginningLocation, endingLocation){
-
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
   };
 };
