5c48bfbbd94356f5601587929fbbc238e1c23976
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 16:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Driver{
 };
 
 
-  const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
 class Route{
 
 
