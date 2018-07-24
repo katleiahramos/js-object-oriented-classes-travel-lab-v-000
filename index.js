4b821c37e81d743737167e5e66079cc2d73155db
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 16:46:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
+
+  yearsExperienceFromBeginningOf(endYear){
+    let end = new Date(endYear, 0, 1);
+    let total =  (end - this.startDate)/(31536000000);//returns in miliseconds
+
+    return Math.round(total);
+  };
+
+};
+
+
+
+class Route{
+  const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+
+
+  };
+
+  aveToIndex(ave){
+    return eastWest.indexOf(ave);
+  };
+
+  blocksTravelled(){
+    const deltaX = this.aveToIndex(this.endingLocation.horizontal) - this.aveToIndex(this.beginningLocation.horizontal);
+
+    const deltaY = this.endingLocation.vertical - this.beginningLocation.vertical;
+
+    return deltaX + deltaY;
+  };
+
+
+
+};
