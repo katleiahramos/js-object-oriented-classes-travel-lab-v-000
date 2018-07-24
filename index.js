a96e4a37963220f4b7f561dce4628802b97ee48c
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 16:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ class Driver{
   };
 
   yearsExperienceFromBeginningOf(endYear){
-    let end = new Date(endYear, 1, 1)
-    return end 
+    let end = new Date(endYear, 0, 1)
+    return end
   };
 
 }
