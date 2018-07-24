62b8a2e4cc75b55a217e52d27875aeeae67fc9af
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 16:33:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ class Route{
   constructor(beginningLocation, endingLocation){
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
+
+    
   };
 
   // blocksTravelled(){
-  //   
+  //
   // }
 
 };
